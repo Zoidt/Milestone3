bef16c096c002d0d70a6137444e55b75fc27ad4d
ZoidT <zaidtabana@gmail.com>
2023-11-20 12:01:27 -0500

    Preparing and Implementing the search service

---
+++ Ecommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Api.Search.Interfaces;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Api.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+
+        public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerID)
+        {
+            await Task.Delay(1);
+            return (true, new { Message = "Hello" });
+        }
+    }
+}

1daaee3bd2ff40100bd40b72abdcd65c3c09b8d8
Zoidt <zaidtabana@gmail.com>
2023-12-17 21:51:42 -0500

    add search swagger documentation

--- Ecommerce.Api.Search/Controllers/SearchController.cs
+++ Ecommerce.Api.Search/Controllers/SearchController.cs
@@ -29,6 +29,68 @@ namespace Ecommerce.Api.Search.Controllers
             this.searchService = searchService;
         }
 
+        /// <summary>
+        /// Get search result using customer id provided
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Returns search term</response>
+        /// <remarks>
+        /// Sample request:
+        /// <code>
+        /// POST /search
+        /// {
+        ///     "customer": {
+        ///         "id": 1,
+        ///         "name": "John Doe",
+        ///         "address": "102 rue barceloone"
+        ///     },
+        ///     "orders": [
+        ///         {
+        ///             "id": 1,
+        ///             "orderDate": "2023-12-17T21:44:20.2343963-05:00",
+        ///             "total": 100,
+        ///             "items": [
+        ///                 {
+        ///                     "id": 1,
+        ///                     "productId": 1,
+        ///                     "productName": "Keyboard",
+        ///                     "quantity": 10,
+        ///                     "unitPrice": 10
+        ///                 },
+        ///                 {
+        ///                     "id": 2,
+        ///                     "productId": 2,
+        ///                     "productName": "Mouse",
+        ///                     "quantity": 10,
+        ///                     "unitPrice": 10
+        ///                 },
+        ///                 {
+        ///                     "id": 3,
+        ///                     "productId": 3,
+        ///                     "productName": "Monitor",
+        ///                     "quantity": 10,
+        ///                     "unitPrice": 10
+        ///                 },
+        ///                 {
+        ///                     "id": 4,
+        ///                     "productId": 2,
+        ///                     "productName": "Mouse",
+        ///                     "quantity": 10,
+        ///                     "unitPrice": 10
+        ///                 },
+        ///                 {
+        ///                     "id": 5,
+        ///                     "productId": 3,
+        ///                     "productName": "Monitor",
+        ///                     "quantity": 1,
+        ///                     "unitPrice": 100
+        ///                 }
+        ///             ]
+        ///         }
+        ///     ]
+        /// }
+        /// </code>
+        /// </remarks>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
